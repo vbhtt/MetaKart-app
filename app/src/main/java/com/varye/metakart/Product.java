168968ef40c308cdb3f67644dcddec2a0bfaf769
Varun <bhattvarun29@gmail.com>
2016-03-07 12:22:14 +0530

    Added Product Card

---
+++ app/src/main/java/com/varye/metakart/Product.java
@@ -0,0 +1,15 @@
+package com.varye.metakart;
+
+/**
+ * Created by Varun on 07-03-2016.
+ */
+public class Product {
+    String productName, brand;
+    int price;
+
+    Product(String productName, String brand, int price){
+        this.productName=productName;
+        this.brand=brand;
+        this.price=price;
+    }
+}
